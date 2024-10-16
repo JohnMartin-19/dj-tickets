9d62b5205530c0e41a783f968c60bbb3afcb3a65
JohnMartin-19 <johnnkonge2020@gmail.com>
2024-10-16 11:49:30 +0300

    Create a django project, a django app and now working on the models

--- ticketing/store/views.py
+++ ticketing/store/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    context = {}
+    return render(request, 'store/landing.html')
+def cart(request):
+    context = {}
+    return render (request, 'store/cart.html',context)
+
+def store(request):
+    context = {}
+    return render (request, 'store/store.html',context)
+
+def checkout(request):
+    context = {}
+    return render (request, 'store/checkout.html',context)
